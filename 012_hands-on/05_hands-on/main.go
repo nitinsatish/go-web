a6c5b5059f09fdb75319b57f173397568349d8e5
Nitin Satish <nitin.satish@fireeye.com>
2018-06-07 12:35:05 +0530

    hands-on 2

--- 012_hands-on/05_hands-on/main.go
+++ 012_hands-on/05_hands-on/main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"text/template"
 )
 
 var tpl *template.Template
+
+type item struct {
+	Name  string
+	Price float64
+}
+type menu struct {
+	Timing string
+	Item   item
+}
 
 func init() {
 	tpl = template.Must(template.ParseGlob("*.gohtml"))
 }
 
 func main() {
+	mymenu := menu{
+		Timing: "Breakfast",
+		Item: item{
+			Name:  "Oats",
+			Price: 24.5,
+		},
+	}
+
+	err := tpl.Execute(os.Stdout, mymenu)
+	if err != nil {
+		fmt.Println("Error !")
+	}
 
 }
